b87574676753ab8388cc11163ccd1947b7e51cf6
Japji Multani <jpjsingh16@gmail.com>
2022-12-24 22:20:18 -0600

    created a button to quit the game from main menu. Escape key can be used to quit the game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         RestartGame();
+        QuitGame();
     }
 
     #endregion
--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -10,5 +10,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1); //Game
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
     #endregion
 }
